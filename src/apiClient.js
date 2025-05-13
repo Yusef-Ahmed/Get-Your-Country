c7238da821559dfde3e3850e13cbf5d2b716674a
Youssef Ahmed <yousseffcai@gmail.com>
2025-05-14 00:14:27 +0300

    Fetch Palestine data and update country filtering logic to include it when "Israel" is encountered

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export async function fetchCountries(url) {
   try {
+    const palestine = await axios.get("https://restcountries.com/v3.1/name/palestine");
     const res = await axios.get(url);
-    return res.data.filter((country) => country.name.common != "Israel");
+    return res.data.map((country) => country.name.common != "Israel" ? country : palestine.data[0]);
   } catch {
     return [];
   }
