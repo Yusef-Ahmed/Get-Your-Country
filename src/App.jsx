363e8ea6a5345173e5a0b827c97168ea89c19e47
Youssef Ahmed <yousseffcai@gmail.com>
2025-05-13 23:39:39 +0300

    Update title in index.html and enhance country display in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => res.data)
-      .then((data) => data.filter((country) => country.name.common != "Israel"))
+      .then((data) => data.filter((country) => country.name.common != "Israel")) // Not in my watch
       .then((data) => setCountries(data))
       .catch(() => setCountries([]));
   }, []);
@@ -22,35 +22,69 @@ function App() {
     axios
       .get(api)
       .then((res) => res.data)
-      .then((data) => data.filter((country) => country.name.common != "Israel"))
+      .then((data) => data.filter((country) => country.name.common != "Israel")) // Not in my watch
       .then((data) => setCountries(data))
       .catch(() => setCountries([]));
   };
 
   return (
     <main className="flex flex-col gap-20">
-      <h1 className="text-center text-5xl mt-10 font-bold">Get Your Country</h1>
-      <section className="flex justify-center gap-5">
+      <h1 className="text-center text-3xl md:text-5xl xl:text-7xl mt-10 font-bold">Get Your Country</h1>
+      <section className="flex justify-center gap-5 text-xl md:text-xl">
         <input
           className="border rounded p-2"
           ref={search}
           name="search"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
         ></input>
-        <button className="bg-purple-800 p-2 rounded" onClick={handleSearch}>
+        <button
+          className="bg-purple-800 p-2 rounded cursor-pointer"
+          onClick={handleSearch}
+        >
           Search
         </button>
       </section>
       {countries.length ? (
         <div className="flex flex-wrap justify-around gap-y-10">
           {countries.map((country, index) => (
-            <div key={index} className="w-md border p-2 rounded-md">
-              <h1 className="text-center text-2xl my-5">
+            <div key={index} className="w-sm border p-2 rounded-md">
+              <h1 className="text-center text-2xl my-3">
                 {country.name.common}
               </h1>
               <img
                 className="h-56 w-full object-contain"
                 src={country.flags.svg}
               ></img>
+              <div className="flex flex-col gap-2 px-2 my-3">
+                <p>
+                  <b>Capital:</b> {country.capital?.join(", ")}
+                </p>
+                <p>
+                  <b>Population:</b> {country.population}
+                </p>
+                <ul className="list-disc list-inside">
+                  <b>Timezones:</b>{" "}
+                  {country.timezones.map((timezone) => (
+                    <li>{timezone}</li>
+                  ))}
+                </ul>
+                <ul className="list-disc list-inside">
+                  <b>Currencies: </b>
+                  {country.currencies &&
+                    Object.entries(country.currencies).map(([key, value]) => {
+                      if (key == "ILS") return; // Not in my watch
+                      return (
+                        <li key={key}>
+                          {key}: {value.name}
+                        </li>
+                      );
+                    })}
+                </ul>
+              </div>
             </div>
           ))}
         </div>
